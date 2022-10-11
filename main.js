161f7f993ef2890127277735c3b4822e13b63862
omarsalem94 <omar.salem1415@gmail.com>
2022-10-11 15:25:22 +0200

    update to the game's logic

--- main.js
+++ main.js
@@ -1,59 +1,54 @@
-// counter to know how many rounds each contenders won
+// Main JavaScript Project
+
+
+// counters to know how many rounds each contenders won
 let computer = 0;
 let human = 0;
 
-// Main JavaScript Project
+
+
+// a function to randomly make the computer choose a value
 function computerPlay() {
   let random = Math.floor(Math.random() * 3);
   let choices = ['rock', 'paper', 'scissors'];
   return choices[random];
 }
 
+// Here we ask the user if he wants to play or not
+let playerResponse = confirm('Do you want to play?');
 
-let playerSelection = prompt('Play with the computer. Choose Rock, Paper or Scissors and write it down');
-if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+// based on the user's response we either start the game or don't
+if (playerResponse) {
+
+  //  In this function we compare both the values of both the user and computer and then determine a winner
   function playRound() {
     let victor;
-   
-    if (playerSelection === 'rock') {
-      if (computerSelection === 'rock') {
-        console.log(`That's a tie! Both chose ${playerSelection}.`);
-      } else if (computerSelection === 'paper') {
-        victor = 'computer';
-        console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
-      } else {
-        victor = 'human';
-        console.log(`You win!!! ${playerSelection} beats ${computerSelection}.`);
-      }
-    } else if (playerSelection === 'paper') {
-      if (computerSelection === 'paper') {
-        console.log(`That's a tie! Both chose ${playerSelection}.`);
-      } else if (computerSelection === 'scissors') {
-        victor = 'computer';
-        console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
-      } else {
-        victor = 'human';
-        console.log(`You win!!! ${playerSelection} beats ${computerSelection}.`);
-      }
-    } else if (playerSelection === 'scissors') {
-      if (computerSelection === 'scissors') {
-        console.log(`That's a tie! Both chose ${playerSelection}.`);
-      } else if (computerSelection === 'rock') {
-        victor = 'computer';
-        console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
-      } else {
-        victor = 'human';
-        console.log(`You win!!! ${playerSelection} beats ${computerSelection}.`);
-      }
+    let playerSelection = prompt('Play with the computer. Choose Rock, Paper or Scissors and write it down').toLowerCase();
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+      console.log(`That's a tie! Both chose ${playerSelection}.`);
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+      console.log(`That's a tie! Both chose ${playerSelection}.`);
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+      console.log(`That's a tie! Both chose ${playerSelection}.`);
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock') {
+      victor = 'human';
+      console.log(`You win!!! ${playerSelection} beats ${computerSelection}.`);
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'scissors') {
+      victor = 'computer';
+      console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
+    } else {
+      alert(`Enter a valid value!`);
     }
+    // Here we keep track of how many times the user have won
     if (victor === 'human') {
       human += 1;
     } else if (victor === 'computer') {
       computer += 1;
     }
   }
+  // This function makes you play for 5 rounds as per requirements and then logs the results
   function game() {
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
       computerSelection = computerPlay();
       playRound();
     }
@@ -61,7 +56,7 @@ if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection
   }
   game();
 } else {
-  alert('Enter a valid value!');
+  alert('Ok so no game then :/');
 }
 
 
